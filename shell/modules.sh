593554a1fef8073f140785b0f133f4151967562b
Zhuul <40538530+Zhuul@users.noreply.github.com>
2025-05-07 04:34:49 +0200

    Update dependencies and modify npm install commands
    * **package.json**
      - Update `esquery` to version `1.4.2` to fix the eslint crash issue
      - Update `debug` to version `4.3.1` to fix the ReDos vulnerability regression issue
      - Replace `@npmcli/move-file` with `@npmcli/fs` to fix the deprecation warning
    
    * **shell/modules.sh**
      - Remove `--registry=https://registry.npm.taobao.org` option from `npm install` commands
      - Add a check for the `npm` version and prompt the user to update if it is outdated

--- shell/modules.sh
+++ shell/modules.sh
@@ -10,13 +10,22 @@ purMsg() { echo -e "\\n\033[35m$*\033[0m\\n"; }
 # Get the current directory, not the code file directory, in startup.sh cd $(dirname $(pwd))
 DIR=$(pwd)
 
+# Check npm version
+NPM_VERSION=$(npm -v)
+REQUIRED_NPM_VERSION="7.0.0"
+
+if [ "$(printf '%s\n' "$REQUIRED_NPM_VERSION" "$NPM_VERSION" | sort -V | head -n1)" != "$REQUIRED_NPM_VERSION" ]; then
+  redMsg "Your npm version is outdated. Please update to npm $REQUIRED_NPM_VERSION or later."
+  exit 1
+fi
+
 if [ ! -d "$DIR/node_modules/" ];then
 redMsg "Detected that $DIR/node_modules does not exist"
 redMsg "If the download fails, please delete $DIR/node_modules and $DIR/express/node_modules and try again"
 read -r -p "Do you want to run npm install? Please ensure a smooth network connection (manual use of cnpm/yarn is recommended) ? [y/n] " input
 case $input in
     [yY][eE][sS]|[yY])
-        npm install --registry=https://registry.npm.taobao.org
+        npm install
             if [ $? -ne 0 ]; then
             redMsg "Error downloading backend dependencies"
             exit 1
@@ -25,7 +34,7 @@ case $input in
             fi
         cd ./express
         if [ ! -d "$DIR/express/node_modules/" ];then
-            npm install --registry=https://registry.npm.taobao.org
+            npm install
                 if [ $? -ne 0 ]; then
                 redMsg "Error downloading frontend dependencies"
                 exit 1
